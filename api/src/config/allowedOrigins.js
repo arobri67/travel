04ce1f1120d3bddb56d09132f1ad379c8920192f
Arnaud <arnaud.obri@proton.me>
2024-10-15 17:51:48 +0200

    Update allowedOrigins.js

--- api/src/config/allowedOrigins.js
+++ api/src/config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ['https://travel-tan-xi.vercel.app'];
+const allowedOrigins = [
+    'https://travel-tan-xi.vercel.app',
+    'https://travel-tan-xi.vercel.app/',
+    'http://travel-tan-xi.vercel.app',
+    'http://travel-tan-xi.vercel.app/',
+];
 
 module.exports = allowedOrigins;
