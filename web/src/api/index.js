f5f2e1bfb66904aa958f2654027e58c614ec44f5
Arnaud <arnaud.obri@proton.me>
2024-10-16 10:15:26 +0200

    Update index.js

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -7,4 +7,27 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// Add a request interceptor
+api.interceptors.request.use(
+  function (config) {
+    console.log('Request headers:', config.headers);
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+api.interceptors.response.use(
+  function (response) {
+    console.log('Response headers:', response.headers);
+    return response;
+  },
+  function (error) {
+    console.error('Response error:', error.response?.data);
+    return Promise.reject(error);
+  },
+);
+
 export default api;
