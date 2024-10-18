a7cdcdc242fe0f77513de77c56d31265cceb8eb8
Arnaud <arnaud.obri@proton.me>
2024-10-18 14:24:10 +0200

    Update indexRouter.js

--- api/src/api/routes/indexRouter.js
+++ api/src/api/routes/indexRouter.js
@@ -6,7 +6,9 @@ const authRoutes = require('./authRoutes');
 const isAuthenticated = require('../../middleware/auth');
 
 const indexRouter = express.Router();
-
+indexRouter.use('/', (req, res) => {
+    res.send('travel-api is up and running!');
+});
 indexRouter.use('/users', isAuthenticated, userRoutes);
 indexRouter.use('/listings', isAuthenticated, listingRoutes);
 indexRouter.use('/auth', authRoutes);
